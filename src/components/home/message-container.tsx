4e0b99051bc43f3eb881b6674e01635b4c2324dc
bytadit <adityabagusp345@gmail.com>
2024-10-11 10:45:46 +0700

    adding chat bubble

--- src/components/home/message-container.tsx
+++ src/components/home/message-container.tsx
@@ -1,13 +1,25 @@
-import { messages } from "@/dummy-data/db";
+// import { messages } from "@/dummy-data/db";
 import ChatBubble from "@/components/home/chat-bubble";
+import { useQuery } from "convex/react";
+import { api } from "../../../convex/_generated/api";
+import { useRoomStore } from "@/store/chat-store";
 
 const MessageContainer = () => {
+	const {selectedRoom} = useRoomStore();
+	const messages = useQuery(api.messages.getMessages, {
+		room: selectedRoom!._id,
+	});
+	const me = useQuery(api.users.getMe);
 	return (
 		<div className='relative p-3 flex-1 overflow-auto h-full bg-chat-tile-light dark:bg-chat-tile-dark'>
 			<div className='mx-12 flex flex-col gap-3 h-full'>
 				{messages?.map((msg, idx) => (
 					<div key={msg._id}>
-						<ChatBubble />
+						<ChatBubble 
+						me={me}
+						message={msg}
+						previousMessage={idx > 0 ? messages[idx - 1] : undefined}
+						/>
 					</div>
 				))}
 			</div>
